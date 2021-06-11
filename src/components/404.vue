e587bf9e0aaaafe8843f89f6dd57d8031ef07d9a
sa-borgia3 <sa.borgia3@gmail.com>
2021-06-11 15:10:08 +0200

    404 not found

---
+++ src/components/404.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="notfound">
+    <div class="notfound__title">
+      <h2>
+        Sorry it's a 404!
+        <br />
+        Not found
+      </h2>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style lang="sass" scoped>
+  @import '../assets/sass/variables'
+  @import '../assets/sass/modules/typography.sass'
+
+
+  .notfound
+    height: 600px
+    width: 100%
+    display: flex
+    justify-content: center
+    align-items: center
+    text-align: center
+
+
+    &__title
+      color: slategray
+      font-size: 33px
+      font-weight: bolder
+</style>
--- src/views/Users/Show.vue
+++ src/views/Users/Show.vue
@@ -2,11 +2,13 @@
   <div>
     <Card v-if="user" :data="user"></Card>
     <!-- v else component not found or 404 -->
+    <NotFound v-else></NotFound>
   </div>
 </template>
 
 <script>
   import Card from "@/components/Card.vue";
+  import NotFound from "@/components/404.vue";
   export default {
     props: {
       data: {
@@ -21,16 +23,15 @@
     },
     components: {
       Card,
+      NotFound,
     },
     computed: {
       user() {
-        return (
-          this.$store.users.list
-            .filter((user) => {
-              return user.id == this.$route.params.id;
-            })
-            .pop() || null
-        );
+        return this.$store.users.list
+          .filter((user) => {
+            return user.id == this.$route.params.id;
+          })
+          .pop();
       },
     },
   };
