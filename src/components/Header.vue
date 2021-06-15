a648207013b513e3e344dbf202270fe0bf0ecf19
sa-borgia3 <sa.borgia3@gmail.com>
2021-06-15 12:10:17 +0200

    z index header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -14,34 +14,35 @@
   </div>
 </template>
 <style lang="sass" scoped>
-@import '../assets/sass/variables'
-@import '../assets/sass/modules/mixins'
-.header
-  +box-shadow(0 0 30px rgba(0,0,0,.2))
-  display: block
-  background: #FFFFFF
-  position: fixed
-  top: 0
-  left: 0
-  width: 100%
-  height: $header_height
-  display: flex
-  align-items: stretch
-
-
-
-  &__content
-    display: flex
-    padding-inline: 20px
+  @import '../assets/sass/variables'
+  @import '../assets/sass/modules/mixins'
+  .header
+    +box-shadow(0 0 30px rgba(0,0,0,.2))
+    display: block
+    background: #FFFFFF
+    position: fixed
+    top: 0
+    left: 0
     width: 100%
-    justify-content: space-between
-    align-items: center
+    height: $header_height
+    display: flex
+    align-items: stretch
+    z-index: 1
 
-  &__content-left
-    flex: none
 
-  &__content-center
 
-  &__content-right
-    flex: none
+    &__content
+      display: flex
+      padding-inline: 20px
+      width: 100%
+      justify-content: space-between
+      align-items: center
+
+    &__content-left
+      flex: none
+
+    &__content-center
+
+    &__content-right
+      flex: none
 </style>
